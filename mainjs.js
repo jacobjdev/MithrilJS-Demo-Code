3367155c70128f9783f5546e3f62a5192588e7f7
Jacob Jensen <jacob@jacobj.org>
2019-12-03 15:21:54 -0600

    Actually got it working

--- mainjs.js
+++ mainjs.js
@@ -1,4 +1,34 @@
 var root = document.body
+var count = 0
 
-m.render(root, "Hello World")
+function init(){
+
+    var Hello = {
+        view: function() {
+            return m("main", [
+                m("h1", {
+                    class: "title"
+                }, "My first app"),
+                m("button", {
+                    onclick: function() {count++}
+                }, count + " clicks"),
+            ])
+        }
+    }
+    
+    var Splash = {
+        view: function() {
+            return m("a", {
+                href: "#!/hello"
+            }, "Enter!")
+        }
+    }
+    
+    m.route(root, "/splash", {
+        "/splash": Splash,
+        "/hello": Hello,
+    })
+
+
+}
 
